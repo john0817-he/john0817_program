4152a392348b9ae7a3bbc7edc1b815375addeea8
john0817 | 존8 <65148388+john0817-he@users.noreply.github.com>
2020-11-01 20:40:00 +0900

    Update No_bad_site.js

--- No_bad_site.js
+++ No_bad_site.js
@@ -11,4 +11,4 @@ arr.forEach(e => {
 WARNING = WARNING || e == m1
 })
 if(WARNING) alert("본 사이트는 악성사이트입니다. 접근이 차단되었습니다.")
-else {let m2 = confirm("안전한 사이트로 추정되지만, 단축링크라면 당신을 속일수도 있습니다. 창을 여는것에 동의하시나요?");if(m2){open(m1)}else{alert("어쩌면 잘 생각한걸지도 몰라요.")};console.log("악성 사이트 방지 프로그램을 다시 시작하려면 F5를 눌러주세요.");import('https://raw.githack.com/john0817-he/john0817/master/No_bad_site.js')');}
+else {let m2 = confirm("안전한 사이트로 추정되지만, 단축링크라면 당신을 속일수도 있습니다. 창을 여는것에 동의하시나요?");if(m2){open(m1)}else{alert("어쩌면 잘 생각한걸지도 몰라요.")};console.log("악성 사이트 방지 프로그램을 다시 시작하려면 F5를 눌러주세요.");window.copy('import('https://raw.githack.com/john0817-he/john0817/master/No_bad_site.js')');}
