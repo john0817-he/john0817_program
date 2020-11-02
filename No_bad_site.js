278cf04ecc71663e1e171eb789b3bde0d66d71ca
john0817 | 존8 <65148388+john0817-he@users.noreply.github.com>
2020-11-02 16:08:51 +0900

    Update No_bad_site.js

--- No_bad_site.js
+++ No_bad_site.js
@@ -1,7 +1,7 @@
 //사이트 링크를 누리기 전에 사이트링크를 여기에 입력해서 악성 사이트인지 알아보세요!
 //악성사이트라면 열리지 않고 악성사이트가 아니라면 사이트가 열린답니다!!
 //사이트 신고 받습니다!! 계속해서 업데이트할게요^^ 현재 제가 피해를 당한 사이트는 전부 추가하였습니다.
-const arr = ['https://blow.ga/vitamin303','https://blow.ga/vitamin303/','https://playentry.org/ds#!/free/5f6336272dea1a00e6cffb42?category=free&rows=20&title=&page=1&currentPage=1&sort=created&isOwnerView=false&search_title=','https://blow.ga/울프나쁨','https://blow.ga/울프나쁨/','https://www.fake.r-e.kr/','https://www.fake.r-e.kr','http://naver.me/5xNPYqk3','http://naver.me/5xNPYqk3/','https://qops.xyz/RGmLjE','https://blow.ga/20725','https://url.kr/AeMji8','https://blow.ga/vitaminface','https://www.itemmania.com','https://itemmania.com','https://qops.xyz/pgcIe7','https://blow.ga/sd','https://youtu.be/N_I2O1RBS38'];
+const arr = ['https://blow.ga/vitamin303','https://blow.ga/vitamin303/','https://playentry.org/ds#!/free/5f6336272dea1a00e6cffb42?category=free&rows=20&title=&page=1&currentPage=1&sort=created&isOwnerView=false&search_title=','https://blow.ga/울프나쁨','https://blow.ga/울프나쁨/','https://www.fake.r-e.kr/','https://www.fake.r-e.kr','http://naver.me/5xNPYqk3','http://naver.me/5xNPYqk3/','https://qops.xyz/RGmLjE','https://blow.ga/20725','https://url.kr/AeMji8','https://blow.ga/vitaminface','https://www.itemmania.com','https://itemmania.com','https://qops.xyz/pgcIe7','https://blow.ga/sd','https://youtu.be/N_I2O1RBS38','https://yandex.com/','https://yandex.com'];
 alert("안전 사이트열기 프로그램(v.1.8.6)을 실행합니다. (복사하여 입력창에 붙여넣기 해도 됩니다. 코드실행이 종료되면 자동으로 코드가 다시 복사됩니다)")
 let m1 = prompt('열고 싶은 사이트의 링크를 입력해주세요. https,http까지 입력해주세요')
 for(let index in arr){
