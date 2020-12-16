65ed63e9c2429f0010aa436ed97c052ca169edfd
john0817 | 존8 <65148388+john0817-he@users.noreply.github.com>
2020-12-16 23:29:47 +0900

    Create tg2.js

---
+++ tg2.js
@@ -0,0 +1,31 @@
+//소스는 웬만하면 퍼가지 마세요.
+//콘솔에 입력하여 실행할수 있습니다.
+alert('TG_2를 이용해 주셔서 감사합니다... 로딩을 시작합니다.(일부 사이트에선 tg를 사용하실수 없습니다.(play스토어,크롬새탭등))')
+console.log('Loading');
+const p = (e) =>{console.log(e)};
+const 프린트 = (e) =>{console.log(e)};
+const 출력 = (e) =>{console.log(e)};
+const p_warn = (e) =>{console.warn(e)};
+const 출력_경고 = (e) =>{console.warn(e)};
+const a = (e) =>{alert(e)};
+const 알림창 = (e) =>{alert(e)};
+const print = (e) =>{console.log(e)};
+const print_warn = (e) =>{console.warn(e)};
+const open = (e) =>{window.open(e)};
+const 열기 = (e) =>{window.open(e)};
+const o = (e) =>{window.open(e)};
+const close = () =>{window.close()};
+const 닫기 = () =>{window.close()};
+const c = () =>{window.close()};
+const href = (e) =>{window.location.href=e};
+const 사이트이동 = (e) =>{window.location.href=e};
+const h = (e) =>{window.location.href=e};
+const google = () =>{window.location.href='https://google.com/'};
+const naver = () =>{window.location.href='https://naver.com/'};
+const daum = () =>{window.location.href='https://daum.net/'};
+const bing = () =>{window.location.href='https://bing.com/'};
+const youtube = () =>{window.location.href='https://youtube.com/'};
+const 입력창 = (e) =>{prompt(e)};
+console.log('Loading_finish');
+alert('로딩이 완료되었습니다. TG코드는 한번만 입력하면 해당 콘솔에서 이용 가능하며 타 사이트로 이동할땐 다시 설치해야할수 있습니다.')
+console.log('✅인증된 TG가 정상 작동중입니다.');
